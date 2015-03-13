11440aa62dd9dd8580e600055fcdf0b3003ef04f
Manu Bezençon <Groupe GoF>
2015-03-13 16:58:04 +0100

    Changements pour menu
    Plusieurs fichiers

--- www/js/auth.js
+++ www/js/auth.js
@@ -59,7 +59,7 @@ angular.module('citizen-engagement.auth', ['angular-storage'])
                     });
 
                     // Go to the issue creation tab.
-                    $state.go('tab.newIssue');
+                    $state.go('app.search');
 
                 }).error(function() {
 
